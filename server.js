5e191bd63e9466663401cb842449593c62d1d129
Prachi Dhunde <prachidhunde@gmail.com>
2025-09-08 12:13:27 +0530

    Added frontend serving in server.js

--- server.js
+++ server.js
@@ -34,3 +34,11 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
 
 
+const path = require("path");
+
+// Serve frontend
+app.use(express.static(path.join(__dirname, "client/build")));
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client/build", "index.html"));
+});
